d8948da8505943a8441ff9367a3c91b6a6c25356
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-11 14:31:11 +0100

    feat: add reactive forms module

--- angular/src/app/modules/options/options.module.ts
+++ angular/src/app/modules/options/options.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { OptionsRoutingModule } from './options-routing.module';
 import { OptionsComponent } from './pages/options/options.component';
 
 @NgModule({
 	declarations: [OptionsComponent],
-	imports: [CommonModule, OptionsRoutingModule],
+	imports: [CommonModule, OptionsRoutingModule, ReactiveFormsModule],
 })
 export class OptionsModule {}
