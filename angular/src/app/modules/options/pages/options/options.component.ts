d254b2c1ff3e82da31fcc1fef0b7da61e141bf96
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-11 14:31:48 +0100

    feat: setup forms for options page with reactive forms module

--- angular/src/app/modules/options/pages/options/options.component.ts
+++ angular/src/app/modules/options/pages/options/options.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'app-options',
 	templateUrl: 'options.component.html',
 	styleUrls: ['options.component.scss'],
 })
-export class OptionsComponent {}
+export class OptionsComponent implements OnInit {
+	textState: string[] = ['present', 'removed']
+
+	optionsForm: FormGroup;
+	monitorForm: FormGroup;
+
+	ngOnInit(): void {
+		this.optionsForm = new FormGroup({
+			'webhookUrl': new FormControl(null),
+		});
+
+		this.monitorForm = new FormGroup({
+			'pageURL': new FormControl(null),
+			'identifier': new FormControl(null),
+			'textToSearch': new FormControl(null),
+			'textState': new FormControl(null)
+		});
+	}
+
+	
+}
