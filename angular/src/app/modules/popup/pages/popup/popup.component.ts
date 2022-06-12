db7e96752b9f3897c549c2bd1b586e950ce5dcc8
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-12 12:25:25 +0100

    feat: allow popup to send webhook to discord server

--- angular/src/app/modules/popup/pages/popup/popup.component.ts
+++ angular/src/app/modules/popup/pages/popup/popup.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { bindCallback } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TAB_ID } from '../../../../providers/tab-id.provider';
+import { EmbedGIF, embed } from '../../../../../../../chrome/src/webhook' 
 
 @Component({
 	selector: 'app-popup',
@@ -25,5 +26,43 @@ export class PopupComponent {
 				)
 			)
 			.toPromise();
+	}
+
+	async sendMessage() {
+		const webhookURL =
+			'https://discord.com/api/webhooks/985144038384607232/pAcxrHzfhbhaMjXFAv75QUFoFtvXPaa_UCOsiy2Eb7lm6xjYhiBqh54Ah7pApZ8Wvr6c';
+
+			try {
+				fetch(webhookURL, {
+					method: 'post',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						// the username to be displayed
+						username: 'AidenExtension',
+						// the avatar to be displayed
+						avatar_url: EmbedGIF.AidenLogo,
+						// contents of the message to be sent
+						// content:
+						// 	'user mention: <@userNumber>, role mention: <@&rolenumber>, channel mention: <#channelnumber>',
+						// enable mentioning of individual users or roles, but not @everyone/@here
+						allowed_mentions: {
+							parse: ['users', 'roles'],
+						},
+						// embeds to be sent
+						embeds: [embed],
+					}),
+				})
+				console.log('success')
+			} catch (error) {
+				console.log('An error occurred', error)
+			} finally {
+				console.log("this will always run")
+			}
+	}
+
+	onOpenOptionsPage() {
+		chrome.runtime.openOptionsPage() 
 	}
 }
