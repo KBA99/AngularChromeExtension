5f047e4cbade40cc69a0d565b108a51ef93d0ce0
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-12 12:26:14 +0100

    feat: allow service worker to send webhook (will be extracted out to a service)

--- chrome/src/serviceWorker.ts
+++ chrome/src/serviceWorker.ts
@@ -1,1 +1,28 @@
-console.log('serviceWorker script loaded');
+import { embed, EmbedGIF } from './webhook';
+
+const webhookURL =
+	'https://discord.com/api/webhooks/985144038384607232/pAcxrHzfhbhaMjXFAv75QUFoFtvXPaa_UCOsiy2Eb7lm6xjYhiBqh54Ah7pApZ8Wvr6c';
+
+// fetch(webhookURL, {
+// 	method: 'post',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 	},
+// 	body: JSON.stringify({
+// 		// the username to be displayed
+// 		username: 'AidenExtension',
+// 		// the avatar to be displayed
+// 		avatar_url: EmbedGIF.AidenLogo,
+// 		// contents of the message to be sent
+// 		// content:
+// 		// 	'user mention: <@279098137484722176>, role mention: <@&496160161459863552>, channel mention: <#508500699458306049>',
+// 		// enable mentioning of individual users or roles, but not @everyone/@here
+// 		allowed_mentions: {
+// 			parse: ['users', 'roles'],
+// 		},
+// 		// embeds to be sent
+// 		embeds: [embed],
+// 	}),
+// })
+// 	.then(() => console.log('success'))
+// 	.catch((error) => console.log('An error occurd', error));
