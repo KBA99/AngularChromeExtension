aeec68002aef9cc24848b9010d54a7a76694e425
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-14 20:13:41 +0100

    feat: add servers component to options module

--- angular/src/app/modules/options/options.module.ts
+++ angular/src/app/modules/options/options.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { OptionsRoutingModule } from './options-routing.module';
 import { OptionsComponent } from './pages/options/options.component';
+import { ServersComponent } from './pages/options/servers/servers.component';
 
 @NgModule({
-	declarations: [OptionsComponent],
+	declarations: [OptionsComponent, ServersComponent],
 	imports: [CommonModule, OptionsRoutingModule, ReactiveFormsModule],
 })
 export class OptionsModule {}
