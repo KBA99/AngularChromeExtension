50654d73407315c55b1dceb5a0fe6ccd733c46a8
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-17 22:23:07 +0100

    feat: delete server from list

--- angular/src/app/modules/options/pages/options/servers/servers.component.ts
+++ angular/src/app/modules/options/pages/options/servers/servers.component.ts
@@ -8,34 +8,44 @@ import { ServerWebhooks } from '../../../options.interface';
 	styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent extends AbstractOptionsDirective implements OnInit, AfterViewInit {
-	serversObject: {}
+	serversObject: {};
 	constructor() {
 		super();
 	}
 
-	allServers: ServerWebhooks[]
+	allServers: ServerWebhooks[];
 
 	ngAfterViewInit(): void {
-		console.log("Firing off")
-		chrome.storage.sync.get('servers', (result: {servers: ServerWebhooks[]}) => {
-			this.allServers = result.servers
+		console.log('Firing off');
+		chrome.storage.sync.get('servers', (result: { servers: ServerWebhooks[] }) => {
+			this.allServers = result.servers;
 		});
+		console.log(this.allServers);
 	}
 
 	override ngOnInit(): void {
-		super.ngOnInit()
+		super.ngOnInit();
 	}
 
 	fetchAllServers() {
-		console.log(this.allServers)
+		console.log(this.allServers);
 	}
 
 	onUpdateWebhook() {
-		this.allServers.push(this.serverForm.value)
-		chrome.storage.sync.set({servers: this.allServers}, () => {
+		this.allServers.push(this.serverForm.value);
+		this.updateChromeServerList();
+	}
+
+	onDeleteServer(server: ServerWebhooks) {
+		this.allServers = this.allServers.filter((element) => {
+			return element !== server;
+		});
+		this.updateChromeServerList();
+	}
+
+	updateChromeServerList() { //Use this method after updating allServers object to keep in sync with chrome
+		chrome.storage.sync.set({ servers: this.allServers }, () => {
 			console.log('Succesfully stored in chrome storage');
 		});
-
-		this.getAllSyncStorage();
 	}
 }
