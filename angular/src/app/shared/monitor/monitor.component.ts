fd26487d1c6d8914c2bb14134950af9912a0b667
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-18 00:58:33 +0100

    feat: add monitor form to monitor component

--- angular/src/app/shared/monitor/monitor.component.ts
+++ angular/src/app/shared/monitor/monitor.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractOptionsDirective } from 'src/app/modules/options/abstract-options.directive';
 
 @Component({
-  selector: 'app-monitor',
-  templateUrl: './monitor.component.html',
-  styleUrls: ['./monitor.component.scss']
+	selector: 'app-monitor',
+	templateUrl: './monitor.component.html',
+	styleUrls: ['./monitor.component.scss'],
 })
-export class MonitorComponent implements OnInit {
+export class MonitorComponent extends AbstractOptionsDirective implements OnInit {
 
-  constructor() { }
+	monitorForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor() {
+		super();
+	}
 
+	ngOnInit(): void {
+		this.monitorForm = new FormGroup({
+			pageURL: new FormControl(null, [Validators.required, Validators.nullValidator]),
+			identifier: new FormControl(null, [Validators.required, Validators.nullValidator]),
+			textToSearch: new FormControl(null, [Validators.required, Validators.nullValidator]),
+			textState: new FormControl(null, [Validators.required, Validators.nullValidator]),
+		});
+
+	}
+
+	onSubmitMonitorConfig() {
+		console.log(this.monitorForm.value);
+		chrome.storage.sync.set({ [this.allSyncKeys.length + 1]: this.monitorForm.value }, () => {
+			console.log('Succesfully stored in chrome storage');
+		});
+		this.getAllSyncStorage();
+	}
 }
