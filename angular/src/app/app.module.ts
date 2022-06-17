3496438bbdcda5d24bc5daceb1a84ebad37e471a
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-18 00:59:37 +0100

    feat: add material UI to app-module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		CommonModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatSelectModule,
+		MatRadioModule,
+		MatCardModule,
+		MatDividerModule,
+		MatInputModule,
+		MatTabsModule,
+		MatIconModule,
+		MatSnackBarModule,
+		MatCheckboxModule,
+		MatButtonModule,
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
