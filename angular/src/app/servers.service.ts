f34d641cfd5379c31a7b1b64a694d31ffdd434de
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-18 14:05:02 +0100

    feat: add servers Service

---
+++ angular/src/app/servers.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class ServersService {
+	constructor() {
+		console.log('Servers service called!');
+	}
+}
