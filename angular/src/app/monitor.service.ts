480c57c46d0f60e2751fbd38c165841a4a5508cd
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-18 13:59:25 +0100

    feat: add monitor config methods to service

--- angular/src/app/monitor.service.ts
+++ angular/src/app/monitor.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
+import { MonitorConfig } from './shared/monitor/monitor.interface';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class MonitorService {
+	allMonitorConfigs: MonitorConfig[] = [];
+	
 	constructor() {
 		console.log("service called!")
 	}
 
+	addMonitorConfig(config: MonitorConfig) {
+		this.allMonitorConfigs.push(config);
+		this.updateChromeMonitorList();
+	}
+
+	deleteMonitorConfig(config: MonitorConfig) {
+		this.allMonitorConfigs = this.allMonitorConfigs.filter((element) => {
+			return element !== config;
+		});
+		this.updateChromeMonitorList();
+	}
+
+
+	updateChromeMonitorList() {
+		//Use this method after updating allServers object to keep in sync with chrome
+		chrome.storage.sync.set({ monitor: this.allMonitorConfigs }, () => {
+			console.log('Succesfully stored in chrome storage');
+		});
+	}
 }
