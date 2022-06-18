ab091663175424d1c7fb1a2eb22eec81a4b8d1d4
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-19 00:40:34 +0100

    feat: extend MonitorComponent into MonitorConfigsComponent

--- angular/src/app/shared/monitor-configs/monitor-configs.component.ts
+++ angular/src/app/shared/monitor-configs/monitor-configs.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+import { MonitorService } from 'src/app/monitor.service';
+import { MonitorComponent } from '../monitor/monitor.component';
 
 @Component({
-  selector: 'app-monitor-configs',
-  templateUrl: './monitor-configs.component.html',
-  styleUrls: ['./monitor-configs.component.scss']
+	selector: 'app-monitor-configs',
+	templateUrl: './monitor-configs.component.html',
+	styleUrls: ['./monitor-configs.component.scss'],
 })
-export class MonitorConfigsComponent implements OnInit {
+export class MonitorConfigsComponent extends MonitorComponent implements OnInit {
+	constructor(override readonly monitorService: MonitorService) {
+		super(monitorService);
+	}
 
-  constructor() { }
+	@ViewChild(MatAccordion) accordion: MatAccordion;
 
-  ngOnInit(): void {
-  }
+	onStartMonitor() {}
 
 }
