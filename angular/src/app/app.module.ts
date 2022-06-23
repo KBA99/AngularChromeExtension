e3e5bb06b6f8cf62872ed38128b95e7c79693705
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-06-23 01:37:16 +0100

    feat: move material imports to shared module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,20 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatExpansionModule } from '@angular/material/expansion';
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
@@ -25,20 +11,6 @@ import { MatExpansionModule } from '@angular/material/expansion';
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		CommonModule,
-		ReactiveFormsModule,
-		MatFormFieldModule,
-		MatSelectModule,
-		MatRadioModule,
-		MatCardModule,
-		MatDividerModule,
-		MatInputModule,
-		MatTabsModule,
-		MatIconModule,
-		MatSnackBarModule,
-		MatCheckboxModule,
-		MatButtonModule,
-		MatButtonToggleModule,
-		MatExpansionModule,
 	],
 	bootstrap: [AppComponent],
 })
